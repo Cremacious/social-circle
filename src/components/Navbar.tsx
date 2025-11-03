2db779efdb89ac20f1dd13bb86be58ef07d6f9cd
cremacious <chrismackall3@gmail.com>
2025-11-03 16:11:23 -0500

    Setup prisma models

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import DesktopNavbar from './DesktopNavbar';
 import MobileNavbar from './MobileNavbar';
 import { currentUser } from '@clerk/nextjs/server';
 // import { syncUser } from '@/actions/user.action';
+import { CircleDotDashed } from 'lucide-react';
 
 async function Navbar() {
   const user = await currentUser();
@@ -12,7 +13,8 @@ async function Navbar() {
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
+          <div className="flex items-center flex-row gap-2">
+            <CircleDotDashed className="h-6 w-6 mb-1" />
             <Link
               href="/"
               className="text-xl font-bold text-primary font-mono tracking-wider"
