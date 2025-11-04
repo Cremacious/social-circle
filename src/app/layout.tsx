f913798e895fccfd67450ed7736094fb89870d0e
cremacious <chrismackall3@gmail.com>
2025-11-04 09:12:33 -0500

    Created actions for notifications, profile, and posts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 import { ThemeProvider } from '@/components/ThemeProvider';
 import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
-// import { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -43,7 +43,6 @@ export default function RootLayout({
             <div className="min-h-screen">
               <Navbar />
               <main className="py-8">
-                {/* container to center the content */}
                 <div className="max-w-7xl mx-auto px-4">
                   <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
                     <div className="hidden lg:block lg:col-span-3">
@@ -54,7 +53,7 @@ export default function RootLayout({
                 </div>
               </main>
             </div>
-            {/* <Toaster /> */}
+            <Toaster />
           </ThemeProvider>
         </body>
       </html>
